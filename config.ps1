3d84800210e6df3a38bc61c6d1fb9c171f48eaa7
Chris Duck <gpduck@whatsupduck.net>
2019-05-02 17:02:35 -0500

    Add SSL cert creation/binding

--- config.ps1
+++ config.ps1
@@ -1,11 +1,29 @@
 Configuration build {
 
-    Import-DscResource -Module PendingReboot, ComputerManagementDsc, xWebAdministration
+    Import-DscResource -Module PendingReboot, ComputerManagementDsc, xWebAdministration, PSDesiredStateConfiguration
 
     Node $AllNodes.NodeName {
 
         LocalConfigurationManager {
             RebootNodeIfNeeded = $true
+        }
+
+        script SSL {
+            GetScript = {
+                Get-ChildItem cert:\LocalMachine\My | Where-Object {
+                    $_.Subject -match "flawless\.com"
+                }
+            }
+            SetScript = {
+                Import-Module PKI
+                New-SelfSignedCertificate -CertStoreLocation Cert:\LocalMachine\My -DnsName "flawless.com"
+            }
+            TestScript = {
+                $Cert = Get-ChildItem cert:\LocalMachine\My | Where-Object {
+                    $_.Subject -match "flawless\.com"
+                }
+                $Cert.Count -gt 0
+            }
         }
 
         PendingReboot checkReboot {
@@ -56,7 +74,14 @@ Configuration build {
             Name            =  $node.websiteName
             PhysicalPath    = 'c:\inetpub\flawlessdotcom'
             State           = 'started'
-            BindingInfo     = $certthumbprint
+            BindingInfo     = @(
+                MSFT_xWebBindingInformation {
+                    Protocol              = "HTTPS"
+                    Port                  = 443
+                    CertificateSubject    = $node.CertSubject
+                    CertificateStoreName  = "MY"
+                }
+            )
             ApplicationPool = 'DefaultAppPool'
             Ensure          = Present
         }
@@ -69,6 +94,7 @@ $ConfigData = @{
             NodeName    = 'localhost'
             MachineName = 'is01'
             websiteName = 'flawlessfaction'
+            CertSubject = 'CN=flawless.com'
         }
     )
 }
