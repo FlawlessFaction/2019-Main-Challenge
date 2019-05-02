c8e75130272e43cc948386b39fb4d15bb2f9a02f
Jeremy Murrah <murrahjm@gmail.com>
2019-05-02 14:45:42 -0700

    dsc configuration push

--- config.ps1
+++ config.ps1
@@ -1,65 +1,74 @@
 Configuration build {
 
-    Import-DscResource -Module PendingReboot, ComputerManagementDsc
+    Import-DscResource -Module PendingReboot, ComputerManagementDsc, xWebAdministration
 
     Node $AllNodes.NodeName {
 
-        LocalConfigurationManager
-        {
+        LocalConfigurationManager {
             RebootNodeIfNeeded = $true
         }
 
-        PendingReboot checkReboot
-        {
+        PendingReboot checkReboot {
             name = "flawless reboots plz"
         }
 
-        Computer SetName
-        {
+        Computer SetName {
             Name = $node.MachineName
         }
 
-        WindowsFeature 'windows-internal-database'
-        {
+        WindowsFeature 'windows-internal-database' {
             Ensure = 'Present'
             Name   = 'Windows-Internal-Database'
         }
 
-        WindowsFeature 'microsoftwindowspowershellv2root'
-        {
+        WindowsFeature 'microsoftwindowspowershellv2root' {
             Ensure = 'absent'
             Name   = 'MicrosoftWindowsPowerShellV2Root'
         }
 
-        WindowsFeature 'rsat-nlb'
-        {
+        WindowsFeature 'rsat-nlb' {
             Ensure = 'present'
             Name   = 'RSAT-NLB'
         }
 
-        WindowsFeature 'nlb'
-        {
+        WindowsFeature 'nlb' {
             Ensure = 'presnet'
             Name   = 'NLB'
         }
 
-        WindowsEventLog ApplicationEventlogSize
-        {
+        WindowsEventLog ApplicationEventlogSize {
             LogName            = 'Application'
             IsEnabled          = $true
             LogMode            = 'Retain'
             MaximumSizeInBytes = 4294967296KB
         }
 
+        xWebSiteDefaults WebServer {
+            Applyto                = 'Machine'
+            Logformat              = 'IIS'
+            LogDirectory           = 'C:\inetpub\logs\LogFiles'
+            TraceLogDirectory      = 'c:\inetpub\logs\logfiles'
+            DefaultApplicationPool = 'DefaultAppPool'
+            AllowSubDirConfig      = 'true'
+        }
+
+        xWebsite FlawlessdotCom {
+            Name            =  $node.websiteName
+            PhysicalPath    = 'c:\inetpub\flawlessdotcom'
+            State           = 'started'
+            BindingInfo     = $certthumbprint
+            ApplicationPool = 'DefaultAppPool'
+            Ensure          = Present
+        }
     }
 }
-
 
 $ConfigData = @{
     AllNodes = @(
         @{
             NodeName    = 'localhost'
             MachineName = 'is01'
+            websiteName = 'flawlessfaction'
         }
     )
 }
